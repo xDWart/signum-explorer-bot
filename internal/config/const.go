59e9b4b99a0fcde98d3d9b00f1d20ba4d7f7e9fb
Anatoliy Bezgubenko <anatoliy.bezgubenko@ikitlab.com>
2021-08-24 22:28:27 +0300

    Fix reward recipient change notifier

--- internal/config/const.go
+++ internal/config/const.go
@@ -3,7 +3,7 @@ package config
 import "time"
 
 const NAME = "<b>🚀 Signum Explorer Bot</b>"
-const VERSION = "<i>v.0.8.3</i>"
+const VERSION = "<i>v.0.8.4</i>"
 
 const (
 	COMMAND_START    = "/start"
